33792e926f45d7c78fd35964750bd921e3b0fe10
Victor França Lopes <victorfranlopes@outlook.com>
2019-04-23 06:30:30 -0300

    ✅ - adding tests(hfxbus): master
    - Creating benchmark

--- test/benchmark/hfx-bus.bench.js
+++ test/benchmark/hfx-bus.bench.js
@@ -7,6 +7,16 @@ const execute = async () => {
 	const committerBus = new HFXBus();
 	committerBus.setClientFactory(HFXBus.factories.ioredis);
 	committerBus.on('error', (error) => console.log(error));
+	const consumerBus = new HFXBus();
+	consumerBus.setClientFactory(HFXBus.factories.ioredis);
+	consumerBus.on('error', (error) => console.log(error));
+
+	await consumerBus.onAwait(
+		'healthz:ping:pending',
+		async (message) => {
+			await message.resolve();
+		}
+	).consume('healthz', ['ping']);
 
 	suite
 		.add('commit message', {
@@ -38,6 +48,7 @@ const execute = async () => {
 		})
 		.on('complete', async () => {
 			await committerBus.stop(false);
+			await consumerBus.stop(false);
 		})
 		.run({
 			async: false
